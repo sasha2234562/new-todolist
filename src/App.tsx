4f0e2009fcfe06c4db8dfc0a27db45f593b180e7
Sasha <skalitsenia@mail.ru>
2023-07-10 13:38:07 +0300

    add todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Todolist} from "./todolist";
+import {v1} from "uuid";
+
 
 function App() {
+
+
+    let todolistIdOne = v1();
+    let todolistIdTwo = v1();
+    let [tasksObj, setTasksObj] = useState({
+        [todolistIdOne]: [
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistIdTwo]: [
+            {id: v1(), title: "Meat", isDone: true,},
+            {id: v1(), title: "Milk", isDone: true},
+            {id: v1(), title: "Cats", isDone: false}
+        ]
+    });
+
+    let [todolists, setTodolists] = useState([
+        {id: todolistIdOne, title: "What to learn", filter: 'active'},
+        {id: todolistIdTwo, title: "I need to bye today", filter: 'completed'},
+    ])
+
     return (
         <div className="App">
+            {todolists.map((item) => {
 
+
+                let tasksForTodolist = tasksObj[item.id]
+                return <Todolist
+                    key={item.id}
+                    title={item.title}
+                    todoId={item.id}
+                    task={tasksForTodolist}
+
+                />
+            })}
         </div>
     );
 }
--- src/todolist.tsx
+++ src/todolist.tsx
@@ -1,9 +1,40 @@
+type Tasktype = {
+    title: string
+    id: string
+    isDone: boolean
+}
+type propsTypeTodolist = {
+    title: string
+    todoId: string
+    task: Array<Tasktype>
+}
 
+export const Todolist = (props: propsTypeTodolist) => {
+    const {
+        title,
+        todoId,
+        task
+    } = props
 
-export const Todolist = ()=> {
+    return (
+        <div className={'Todolist'}>
+            <h3>{title}</h3>
+            <input/>
+            <button>+</button>
 
+            {task.map((item) => {
+                return (
+                    <li>
+                        <input
+                            type={"checkbox"}
+                            checked={item.isDone}
+                        />
+                        <span>{item.title}</span>
+                        <button>X</button>
+                    </li>
+                )
+            })}
 
-    return(
-        <div></div>
+        </div>
     )
 }
