36b4a8902041bbddcc9a5d5154f30f419f84c6a9
Sasha <skalitsenia@mail.ru>
2023-08-31 21:42:50 +0300

    12 - Todolist for student (storybook, decorator, snapshot testing)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+import * as path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -8,6 +9,21 @@ const config: StorybookConfig = {
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+           test: [/\.stories\.tsx?$/]
+
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false,
+          options:{
+            parser: 'typescript'
+          }},
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-webpack5",
