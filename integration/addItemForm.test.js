3343907fe1660cd781139eeaab76260cee0afd8b
Sasha <skalitsenia@mail.ru>
2023-09-01 22:47:53 +0300

    12 - Todolist for student (storybook, decorator, snapshot testing)

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -1,9 +1,13 @@
 describe('addItemForm', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
-        await page.goto('http://localhost:9009/iframe.html?args=&id=additemform--add-item-form-bace-example&viewMode=story',
+
+        // eslint-disable-next-line no-undef
+        await page.goto('http://localhost:9009/iframe.html?id=additemform--add-item-form-bace-example&viewMode=story',
             {waitUntil: "networkidle2"});
 
+
+        // eslint-disable-next-line no-undef
         const image = await page.screenshot();
 
         // API from jest-image-snapshot
