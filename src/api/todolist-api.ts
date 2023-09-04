f9a76324dda76f2667f71c252548d1343c4d54e3
Sasha <skalitsenia@mail.ru>
2023-09-04 23:14:59 +0300

    13 - 01 - Todolist for student (api, axios, rest api)

---
+++ src/api/todolist-api.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const settings = {
+    withCredentials: true,
+    headers: {
+        // Не забываем заменить API-KEY на собственный
+        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41',
+    },
+}
+
+export const todolistAPI = {
+    updateTodolist(todolistId: string, title: string) {
+        const promise = axios.put(
+            `https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,
+            { title: title },
+            settings
+        )
+        return promise
+    },
+}
