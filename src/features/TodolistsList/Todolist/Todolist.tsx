6b4a1392d5d4837be61d4fb7450eaea8aefd2887
Sasha <skalitsenia@mail.ru>
2023-09-21 16:51:37 +0300

    14 - 03 - Todolist for student (refactoring before 15 lesson)

--- src/features/TodolistsList/Todolist/Todolist.tsx
+++ src/features/TodolistsList/Todolist/Todolist.tsx
@@ -1,14 +1,14 @@
 import React, {useCallback, useEffect} from 'react'
-import {AddItemForm} from './AddItemForm'
-import {EditableSpan} from './EditableSpan'
+import {AddItemForm} from '../../../components/AddItemForm/AddItemForm'
+import {EditableSpan} from '../../../components/EditableSpan/EditableSpan'
+import {Task} from './Task/Task'
+import {TaskStatuses, TaskType} from '../../../api/todolists-api'
+import {FilterValuesType} from '../todolists-reducer'
+import {fetchTasksTC} from '../tasks-reducer'
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
-import { Delete } from '@mui/icons-material';
-import {Task} from './Task'
-import {TaskStatuses, TaskType} from './api/todolists-api'
-import {FilterValuesType} from './state/todolists-reducer'
-import {useDispatch} from "react-redux";
-import {fetchTasksThunk} from "./state/tasks-reducer";
+import {Delete} from '@mui/icons-material';
+import {useAppDispatch} from "../../../app/store";
 
 type PropsType = {
     id: string
@@ -27,12 +27,14 @@ type PropsType = {
 
 export const Todolist = React.memo(function (props: PropsType) {
     console.log('Todolist called')
-    const dispatch = useDispatch()
-    useEffect(() => {
 
-        // @ts-ignore
-        dispatch(fetchTasksThunk(props.id))
-    }, []);
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        const thunk = fetchTasksTC(props.id)
+        dispatch(thunk)
+    }, [])
+
     const addTask = useCallback((title: string) => {
         props.addTask(title, props.id)
     }, [props.addTask, props.id])
@@ -68,10 +70,10 @@ export const Todolist = React.memo(function (props: PropsType) {
         <div>
             {
                 tasksForTodolist.map(t => <Task key={t.id} task={t} todolistId={props.id}
-                                          removeTask={props.removeTask}
-                                          changeTaskTitle={props.changeTaskTitle}
-                                          changeTaskStatus={props.changeTaskStatus}
-                    />)
+                                                removeTask={props.removeTask}
+                                                changeTaskTitle={props.changeTaskTitle}
+                                                changeTaskStatus={props.changeTaskStatus}
+                />)
             }
         </div>
         <div style={{paddingTop: '10px'}}>
