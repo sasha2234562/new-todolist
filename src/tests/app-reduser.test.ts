a182887da9b7895b21676fca869c37370f6f1d3d
Sasha <skalitsenia@mail.ru>
2023-09-24 17:41:24 +0300

    15 - 02 - Todolist for student (Progressbar and error handling (part 2))

--- src/tests/app-reduser.test.ts
+++ src/tests/app-reduser.test.ts
@@ -1,4 +1,6 @@
 import {appReducer, InitialStateType, setAppErrorAC, setAppStatusAC} from "../app/app-reduser";
+import {v1} from "uuid";
+import {removeTodolistAC, TodolistDomainType, todolistsReducer} from "../features/TodolistsList/todolists-reducer";
 
 
 test('correct error should be sed', () => {
@@ -17,4 +19,17 @@ test('correct status should be set', () => {
     }
     const endState = appReducer(startState, setAppStatusAC('loading'))
     expect(endState.status).toBe('loading')
+})
+
+test('correct entity status of todolist should be changed', () => {
+    const todolistId1: string = v1()
+    const todolistId2: string = v1()
+    const startState: TodolistDomainType[] = [
+        {id: todolistId1, title: 'What to learn', filter: 'all', entityStatus: "idle", order: 0, addedDate: ""},
+        {id: todolistId2, title: 'What to buy', filter: 'all', entityStatus: "idle", order: 0, addedDate: ""}
+    ]
+    const endState = todolistsReducer(startState, removeTodolistAC(todolistId1));
+
+    expect(endState.length).toBe(1)
+    expect(endState[0].id).toBe(todolistId2)
 })
